68390fe5a4d6680ea5bdb9587e8067e581072dba
Maël Abondo <emilemaelnyeckabondo@gmail.com>
2025-12-13 04:20:40 +0100

    modification de la couleur de fond e la fenetre et de l'orloge pour un premier test concluant

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,10 @@ int main() {
     TTF_Font* font = TTF_OpenFont("police/Montserrat/static/Montserrat-ExtraBoldItalic.ttf", 50);
 
     // Créer une surface texte
-    SDL_Color color = {255, 255, 255, 255}; // blanc
+    SDL_Color color = {255, 255, 25, 255}; // blanc
     std::string chrono;
-    int minutes = 20, secondes = 60;
+    int minutes = 20;
+    int secondes = 60;
     SDL_Surface* surface = TTF_RenderText_Blended(font, "20:00",SDL_strlen("20:00"), color);
 
     // Convertir en texture
@@ -25,7 +26,7 @@ int main() {
     // Récupérer dimensions
     float w, h;
     SDL_GetTextureSize(texture, &w, &h);
-    SDL_FRect dst = {400, 300, (float)w, (float)h};
+    SDL_FRect dst = {300, 300, w, h};
 
     bool running = true;
     SDL_Event event;
@@ -34,26 +35,28 @@ int main() {
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_EVENT_QUIT) running = false;
         }
+        //Variation du compteur: temps defini arbitrairement
         chrono = std::to_string(minutes) + " : ";
         chrono += std::to_string(secondes);
         SDL_Surface* surface = TTF_RenderText_Blended(font, chrono.c_str(),SDL_strlen(chrono.c_str()), color);
         secondes -= 0.0002;
+        //modification du compteur apres une minute
         if (secondes <= 0){
             minutes -=0.0002;
             secondes = 60;
         }
         
-        // Convertir en texture
+        // Convertion de la durface en texture
         texture = SDL_CreateTextureFromSurface(renderer, surface);
         SDL_DestroySurface(surface);
-
-        // Récupérer dimensions
         SDL_GetTextureSize(texture, &w, &h);
-        SDL_FRect dst = {400, 300, (float)w, (float)h};
+        SDL_FRect dst = {300, 300, w, h};
 
-        SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+        //dessin remplissage de la fenetre avec la couleur verte
+        SDL_SetRenderDrawColor(renderer, 20, 130, 0, 255);
         SDL_RenderClear(renderer);
 
+        //affichage des textures
         SDL_RenderTexture(renderer, texture, nullptr, &dst);
 
         SDL_RenderPresent(renderer);
@@ -61,6 +64,7 @@ int main() {
         SDL_Delay(1000);
     }
 
+    //nettoyage (je suis bien élevé)
     SDL_DestroyTexture(texture);
     TTF_CloseFont(font);
     SDL_DestroyRenderer(renderer);
